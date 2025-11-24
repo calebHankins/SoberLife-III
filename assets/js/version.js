25f631ab29c537fb62c5b58c37a12bea89b1e358
Caleb Hankins <chanki@chanki.dev>
2025-11-24 13:45:36 -0600

    chore: release v0.27.0

--- assets/js/version.js
+++ assets/js/version.js
@@ -1,6 +1,6 @@
 // Auto-generated version file
 // This file is automatically updated by release-it
-export const VERSION = '0.26.2';
-export const GIT_HASH = 'f9c0f3d';
+export const VERSION = '0.27.0';
+export const GIT_HASH = '489a39e';
 export const GIT_BRANCH = 'main';
-export const BUILD_DATE = '2025-11-23T23:01:34.416Z';
+export const BUILD_DATE = '2025-11-24T19:45:35.691Z';
