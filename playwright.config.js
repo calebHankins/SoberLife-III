3b0d9c9b77a320abf0eabfdcb2a18666c4a3313d
Caleb Hankins <chanki@chanki.dev>
2025-11-26 11:55:15 -0600

    dial back non-ci playwright retries to 0

--- playwright.config.js
+++ playwright.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
     testDir: './tests/playwright',
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 1,
+    retries: process.env.CI ? 2 : 0,
     workers: process.env.CI ? 1 : '25%',
     reporter: process.env.CI ? [['github'], ['html', { open: 'never' }]] : [['list'], ['html', { open: 'never' }]],
 
